9cda22d636c60b144c350ea323def3da18a38276
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-20 16:10:27 +0900

    release

--- data-platform-business-user-sql-business-user-data.sql
+++ data-platform-business-user-sql-business-user-data.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `data_platform_business_user_business_user_data`
 (
-  `BusinessPartner`        int(10) NOT NULL,
-  `BusinessUserID`         int(10) NOT NULL,
-  `ValidityEndDate`        varchar(80) NOT NULL,
-  `ValidityStartDate`      varchar(80) DEFAULT NULL,
-  `BusinessUserFullName`   varchar(200) DEFAULT NULL,
-  `BusinessUserFirstName`  varchar(100) DEFAULT NULL,
-  `BusinessUserLastName`   varchar(100) DEFAULT NULL,
-  `Language`               varchar(2) DEFAULT NULL,
-    PRIMARY KEY (`BusinessPartner`, `BusinessUserID`, `ValidityEndDate`)
+  `BusinessPartner`          int(10) NOT NULL,
+  `ValidityEndDate`          varchar(80) NOT NULL,
+  `ValidityStartDate`        varchar(80) DEFAULT NULL,
+  `BusinessPartnerFullName`  varchar(200) DEFAULT NULL,
+  `BusinessPartnerName`      varchar(100) DEFAULT NULL,
+  `BusinessUserFirstName`    varchar(100) DEFAULT NULL,
+  `BusinessUserLastName`     varchar(100) DEFAULT NULL,
+  `Language`                 varchar(2) DEFAULT NULL,
+    PRIMARY KEY (`BusinessPartner`, `ValidityEndDate`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;

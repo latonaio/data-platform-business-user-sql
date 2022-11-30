e5c09895d1fe151735a329299184d94719dcbb3e
Yurino Oga <yurino.o@latona.io>
2022-11-30 23:35:45 +0900

    release

--- data-platform-business-user-sql-business-user-data.sql
+++ data-platform-business-user-sql-business-user-data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `data_platform_business_user_business_user_data`
 (
+    `EmailAddress`             varchar(200) NOT NULL,
     `BusinessPartner`          int(12) NOT NULL,
-    `BusinessUser`             int(12) NOT NULL,
     `ValidityEndDate`          date NOT NULL,
     `ValidityStartDate`        date NOT NULL,
     `BusinessUserFirstName`    varchar(100) DEFAULT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `data_platform_business_user_business_user_data`
     `BusinessUserFullName`     varchar(100) DEFAULT NULL,
     `Language`                 varchar(2) DEFAULT NULL,
 
-    PRIMARY KEY (`BusinessPartner`, `BusinessUser`, `ValidityEndDate`, `ValidityStartDate`),
+    PRIMARY KEY (`EmailAddress`, `ValidityEndDate`, `ValidityStartDate`),
     
     CONSTRAINT `DPFMBusinessUserBusinessUserDataBusinessPartner_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `data_platform_business_user_business_partner_data` (`BusinessPartner`),
     CONSTRAINT `DPFMBusinessUserBusinessUserDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
